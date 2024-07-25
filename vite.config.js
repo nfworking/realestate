9c70791f2b81f58171ccd7a8d0a64010fe6fa52a
nfworking <168704009+nfworking@users.noreply.github.com>
2024-07-26 08:36:22 +1000

    changed to fit dirs
    changed vite config and also changed dirs

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const root = resolve(__dirname, 'pages')
+const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
