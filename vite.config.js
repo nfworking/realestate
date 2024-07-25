8a86bdf441b25117a9558d5038a1d75f5012279f
nfworking <168704009+nfworking@users.noreply.github.com>
2024-07-26 08:25:14 +1000

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const root = resolve(__dirname, 'pages')
-const ourDir = resolve(__dirname, 'dist')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
