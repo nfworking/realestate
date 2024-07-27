692be8064c4f00d2ab495e982a613e4db7c60feb
nfworking <168704009+nfworking@users.noreply.github.com>
2024-07-27 11:44:06 +1000

    chanaged photos and json photo
    did this

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
